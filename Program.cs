e90e7845b3e8dcf0d2c5c0565ebf8ebb4599ae61
Amanda Vergilio <81338708+Amanda-Arya@users.noreply.github.com>
2021-12-20 13:45:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace conversaoTempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculartempo();
+
+        }
+        static void Calculartempo()
+        {
+            Console.Clear();
+            int time, timeHours, timeMinutes, timeSeconds;
+
+            Console.WriteLine("Qual tempo você deseja calcular?");
+            time = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{time} segundos");
+
+            timeHours = time / 3600;
+            //ex: 556 seg / 3660 = 0,1544
+
+            timeSeconds = time - (timeHours * 3600);
+            // 556 - (0,15 * 3600) = 556 - 540 = 16
+
+            timeMinutes = timeSeconds / 60;
+            // 16/60 =0,26
+
+            timeSeconds = timeSeconds - (timeMinutes * 60);
+
+
+
+
+            Console.WriteLine($"{timeHours}:{timeMinutes}:{timeSeconds}");
+
+        }
+
+    }
+}
+
